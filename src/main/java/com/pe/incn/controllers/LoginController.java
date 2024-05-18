82a4f623965ae047efa01de56f307b3b93a4818f
BrayanTorres <brayantorresacuna2023@gmail.com>
2024-05-18 10:18:19 -0500

    Agregar método para cerrar sesión

--- src/main/java/com/pe/incn/controllers/LoginController.java
+++ src/main/java/com/pe/incn/controllers/LoginController.java
@@ -2,6 +2,9 @@ package com.pe.incn.controllers;
 
 import com.pe.incn.models.Usuario;
 import com.pe.incn.services.UsuarioServiceImpl;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,28 +15,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private UsuarioServiceImpl usuarioService;
+	@Autowired
+	private UsuarioServiceImpl usuarioService;
 
-    @GetMapping("/login")
-    public String showLoginForm(){
-        return "login";
-    }
+	@GetMapping("/login")
+	public String showLoginForm() {
+		return "login";
+	}
 
-    @PostMapping("/login")
-    public String processLogin(@RequestParam String usuario, @RequestParam String contra, Model model){
+	@PostMapping("/login")
+	public String processLogin(@RequestParam String usuario, @RequestParam String contra, Model model,
+			HttpSession session) {
 
-        Usuario usuarioLogin = usuarioService.login(usuario, contra);
+		Usuario usuarioLogin = usuarioService.login(usuario, contra);
 
-        if (usuarioLogin != null){
-            System.out.println("login exitoso");
-            model.addAttribute("usuarioLogin", usuarioLogin);
-            return "redirect:/home";
-        }else {
-            model.addAttribute("error", "Usuario o contraseña incorrectos. Inténtelo de nuevo.");
-            return "login";
-        }
+		if (usuarioLogin != null) {
+			System.out.println(usuarioLogin.getUsername());
+			// model.addAttribute("usuarioLogin", usuarioLogin);
+			session.setAttribute("usuarioLogin", usuarioLogin);
+			return "redirect:/home";
+		} else {
+			model.addAttribute("error", "Usuario o contraseña incorrectos. Inténtelo de nuevo.");
+			return "login";
+		}
 
-    }
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/login";
+	}
 
 }
